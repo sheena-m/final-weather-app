eae942373d04f2c6516b1c482d7e87dee0b18233
sheena-m <52721260+sheena-m@users.noreply.github.com>
2020-09-07 17:25:16 +0100

    Added forecast

--- src/index.js
+++ src/index.js
@@ -20,6 +20,19 @@ function formatDate(date) {
   let currentDay = days[date.getDay()];
   return `${currentDay} ${hours}:${minutes}`;
 }
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
 
 function displayWeatherCondition(response) {
   console.log(response.data);
@@ -36,10 +49,40 @@ function displayWeatherCondition(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function dispalyForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="card">
+      <h3>
+        ${formatHours(forecast.dt * 1000)}
+      </h3>
+      <img
+        src="http://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+        }@2x.png"
+      />
+      <div class="weather-forecast-temperature">
+        <strong>
+          ${Math.round(forecast.main.temp_max)}°C
+        </strong> |
+        ${Math.round(forecast.main.temp_min)}°C
+      </div>
+    </div>
+  `;
+  }
+}
 function searchCity(city) {
   let apiKey = "4a9329eb94e7f0688b257952716e92b5";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(dispalyForecast);
 }
 
 function handleSubmit(event) {
