644c1aab01409f99a99cf58c0450473b580cdb14
sheena-m <52721260+sheena-m@users.noreply.github.com>
2020-09-07 16:15:45 +0100

    Added icon from API

--- src/index.js
+++ src/index.js
@@ -29,6 +29,12 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#weather-description").innerHTML =
     response.data.weather[0].description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 function searchCity(city) {
   let apiKey = "4a9329eb94e7f0688b257952716e92b5";
